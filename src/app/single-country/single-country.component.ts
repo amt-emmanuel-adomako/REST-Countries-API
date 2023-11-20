cb09c66f2bf801c14637c72240ebd103966c9c31
ecy20 <e.adomako@amilitech.org>
2023-11-19 20:49:08 -0800

    single component update 4

--- src/app/single-country/single-country.component.ts
+++ src/app/single-country/single-country.component.ts
@@ -8,6 +8,8 @@ import { DataService } from "../services/data.service";
   styleUrls: ['./single-country.component.scss']
 })
 export class SingleCountryComponent implements OnInit{
+  currency:any ;
+  nativeName:any;
   constructor(private router:Router, public dataService: DataService){}
 
   ngOnDestroy(){
@@ -16,8 +18,10 @@ export class SingleCountryComponent implements OnInit{
   ngOnInit(): void {
     console.log('country component loaded');
     console.log(this.dataService.country);
-    console.log(this.dataService.neighbourhingCountries);
-    
+    // console.log(this.dataService.country.name.nativeName[`${Object.keys(this.dataService.country.name.nativeName).pop()}`].common);
+    // console.log(Object.keys(this.dataService.country.name.nativeName).pop());
+    this.currency = this.dataService.country.currencies[Object.keys(this.dataService.country.currencies)[0]].name
+    this.nativeName = this.dataService.country.name.nativeName[`${Object.keys(this.dataService.country.name.nativeName).pop()}`].common
     if (!this.dataService.country) {
       this.router.navigate(['home'])
     }
